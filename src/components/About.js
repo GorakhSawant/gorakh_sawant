0c593a6e9ec6295d89393bc381906567ba4b956d
GorakhSawant <gorakh.r.sawant@gmail.com>
2024-12-22 14:52:00 +0530

    restructured the structure

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section>
+            <h2>About Me</h2>
+            <p>Welcome to my portfolio! I am a software engineer with a passion for building web applications.</p>
+        </section>
+    );
+};
+
+export default About;
